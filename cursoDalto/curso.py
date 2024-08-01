b5cbf6bae8d018ad8d5dc8d2ca75b0fec0847ab1
Delfiboya <delfiboya@gmail.com>
2024-08-01 12:56:07 -0300

    Datos compuestos

--- cursoDalto/curso.py
+++ cursoDalto/curso.py
@@ -34,4 +34,29 @@ print (cuenta)
 print ("ola" in bienvenida)
 print ("Pedro" in bienvenida)
 
- 
+# definiendo una variable con snake_case
+nombre_completo_tuyo = "Delfina Boyadjian Varde"
+
+
+# Datos compuestos
+lista = ["primero",2,False]
+print (lista)
+print (lista [2])
+tupla = ("primero",2,False)
+
+# modificando una lista (la tupla NO se puede modificar)
+lista [1] = 3
+print (lista)
+
+conjunto = {"primero",2,False}  # no se puede modificar tampoco
+print (conjunto) 
+# print (conjunto [1]) no se puede 
+# conjunto = {"primero","primero"} no se pueden repetir datos, en las listas y en las tuplas si
+
+diccionario = {
+    'nombre' : "Delfina",
+    'edad' : 19,
+    'pelo_negro' : False
+}
+print (diccionario)
+print (diccionario ['edad'])
