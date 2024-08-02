5c94c0a1de5632480ca0a25b77b6a1c8d1716a23
Delfiboya <delfiboya@gmail.com>
2024-08-02 16:04:09 -0300

    Hasta input

---
+++ cursoDalto/input.py
@@ -0,0 +1,15 @@
+# Pedirle al usuario un nombre
+#nombre = input ("Escribir tu nombre: ")
+#print (f"Hola {nombre}")    LO COMENTO PQ MOLESTA EN LA TERMINAL
+
+# Pedirle al usuario un número
+# Ejemplo de un error
+numero = input ("Escribir un número: ")
+#respuesta = numero * 2
+#print (respuesta)
+# devuelve 1010 y no 20
+# para arregarlo entonces usamos la función int
+#respuesta2 = int(numero) * 2
+#print (respuesta2)
+respuesta3 = float (numero) * 2
+print (respuesta3)
