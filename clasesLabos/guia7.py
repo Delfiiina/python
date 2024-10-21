dc025cb9b6ad8ec721cecb690a3843d664ac9a2a
Delfiboya <delfiboya@gmail.com>
2024-10-21 00:15:29 -0300

    Ej 1

--- clasesLabos/guia7.py
+++ clasesLabos/guia7.py
@@ -1,7 +1,7 @@
 from typing import List, Dict, Tuple
 
 # Ejercicio 1
-# 1
+# 1)
 
 def pertenece (numero:int, lista:List[int]) -> bool:
     i: int = 0
@@ -13,5 +13,40 @@ def pertenece (numero:int, lista:List[int]) -> bool:
     else :
         return False
 
-print (pertenece (5, [1,2,3]))
-    
+#print (pertenece (5, [1,2,3]))
+
+def pertenece2 (e:int, s:List [int]) -> bool:
+    mi_numero : int = e
+    mi_lista : List = s
+    for i in range (0,len(mi_lista),1):
+        if mi_lista[i] == mi_numero:
+            return True 
+    return False
+        
+#print (pertenece2 (1, [2,1,3]))
+#print (pertenece2 (5, [1,2,3,5,4])) 
+
+def pertenece3 (e:int, s:List [int]) -> bool:
+    mi_numero : int = e
+    mi_lista : List = s
+    return mi_numero in mi_lista
+
+#print (pertenece3 (1,[2,1,3]))
+#print (pertenece3 (5,[2,1,3])) 
+ 
+
+def pertenece4 (e:int, s:List [int]) -> bool:
+    mi_numero : int = e
+    mi_lista : List = s
+    x : int = 0
+    while x < len(mi_lista):
+        if mi_lista[x] != mi_numero:
+            x += 1
+        else : return True
+    return False
+
+print (pertenece4 (1, [2,3,1,4,6]))
+print (pertenece4 (3, [1,1,1,1])) 
+
+
+# 2)
