9607b241cbe7fcae6d078e9cfa4156672fe94e13
Delfiboya <delfiboya@gmail.com>
2024-11-10 15:14:55 -0300

    sigo hasta el 4

--- clasesLabos/Integrador.py
+++ clasesLabos/Integrador.py
@@ -21,13 +21,49 @@ def palabras_separadas (nombre_archivo: str, caracter: str ) -> list [str]:
             respuesta.append(palabra_actual) # Por cada palabra, la meto a la lista ya que la fui guardando 
             palabra_actual = "" # borro palabra 
         else:
-            palabra_actual += caracter # Voy guardando cada palabra
+            palabra_actual += letra # Voy guardando cada palabra
     respuesta.append(palabra_actual)
     archivo.close() # Cierro el archivo
     return respuesta
-    
-print (palabras_separadas("conceptos1.txt","."))
+
+#prueba = palabras_separadas ("conceptos1.txt",".")
+#print (prueba)
+
 
 # 2) Utilizar la función anterior para los dos archivos provistos y generar una lista de strings para cada uno
+
+#print (palabras_separadas(r"C:\Delfina\python\apartePy\python\clasesLabos\conceptos1.txt","."))
+#print (palabras_separadas(r"C:\Delfina\python\apartePy\python\clasesLabos\conceptos2.txt",";"))
+
+# 3) Generar dos diccionarios, uno para cada lista del punto 2, donde la clave sea el índice de la palabra en la lista y el valor sea la palabra.
+
+def generar_diccionario (lista_de_palabras : list[str]) -> dict[int,str]:
+    indice: int = 0 # el índice tiene que empezar en 0
+    diccionario : dict [int,str] = {}  
     
-# Sin separar por nada(?)
+    
+    for palabra in lista_de_palabras:
+        diccionario [indice] = palabra # como el índice no se va a repetir, no hace falta chequear si ya está la clave 
+        indice += 1
+    
+    return diccionario
+
+print (generar_diccionario(palabras_separadas(r"C:\Delfina\python\apartePy\python\clasesLabos\conceptos1.txt",".")))
+
+# 4) Implementar una función que, dados dos diccionarios como los del punto 3, devuelva una lista de dos listas de enteros, 
+# donde cada una de las listas interiores corresponde a los índices correspondientes a los valores en común entre ambos. 
+# Por ejemplo, si el diccionario A es {1:’silla’, 13: ‘canguro’} y B es {3:’canguro’, 9:’pera’}, la función devuelve [[13], [3]].
+
+def indices_en_comun (diccionario1: dict[int,str], diccionario2: dict [int,str]) -> list[list[int]]:
+    lista1 : list [int] = []
+    lista2: list [int] = []
+    res: list[list[int]] = []
+    indice_actual: int = 0
+    
+    for par_clave_valor in diccionario1:
+        indice_actual = par_clave_valor[0]
+        if par_clave_valor[1] in diccionario2.values():
+            lista1.append(par_clave_valor[0])
+            lista2.append()
+            
+    
