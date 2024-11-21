dca502d7dcf45158d454707e59571c438def3028
Delfiboya <delfiboya@gmail.com>
2024-11-21 12:21:23 -0300

    Hasta el 3

--- clasesLabos/simulacrov2.py
+++ clasesLabos/simulacrov2.py
@@ -35,5 +35,61 @@ def gestion_ventas (ventas_empleado_producto : list [tuple[str,str,int]]) -> dic
 # Ejercicio 2
 
 def cantidad_digitos_impares (numeros:list[int]) -> int:
+    res : list[int] = []
     
-            
+    for numero in numeros:
+        while numero > 0:
+            if (numero%10) % 2 != 0:
+                res.append((numero%10))
+            numero = numero // 10
+     
+    return len(res)
+
+#print (cantidad_digitos_impares([26,13,111,0,268,9]))       
+ 
+# Ejercicio 3
+
+def reordenar_cola_primero_numerosas(carpetas: Cola[tuple[str,int]], umbral:int) -> Cola[tuple[str,int]]:
+    res : Cola[tuple[str,int]] = Cola()
+    carpetas_aux : Cola[tuple[str,int]] = Cola()
+    menor_igual_umbral : list [tuple[str,int]] = []
+    mayores_umbral : list [tuple[str,int]] = []
+    
+    while not carpetas.empty():
+        carpeta : tuple[str,int] = carpetas.get()
+        carpetas_aux.put(carpeta)
+        if carpeta[1] > umbral:
+            mayores_umbral.append(carpeta)
+        else:
+            menor_igual_umbral.append(carpeta)
+    
+    for carpeta in mayores_umbral:
+        res.put(carpeta)
+    for carpeta in menor_igual_umbral:
+        res.put(carpeta)
+        
+    while not carpetas_aux.empty():
+        carpeta : tuple[str,int] = carpetas_aux.get()
+        carpetas.put(carpeta)
+    
+    return res
+
+colaa = Cola()
+colaa.put(("a",6))
+colaa.put(("b",1))
+colaa.put(("c",4))
+colaa.put(("d",10))
+colaa.put(("e",8))
+colaa.put(("f",1))
+
+# RTA ESPERADA ([('a', 6), ('d', 10), ('e', 8), ('b', 1), ('c', 4), ('f', 1)])
+#print (colaa.queue)
+#print (reordenar_cola_primero_numerosas(colaa,5).queue)
+#print (colaa.queue)
+
+# Ejercicio 4
+    
+def esta_ordenada_decreciente (l:list[int]) -> bool:
+    
+
+def matriz_cuasi_decreciente(matriz: list[list[int]]) -> bool:
